28a3cecbd8b3e317a1485a647216c480a4b4c850
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-07-03 15:36:18 -0400

    init

---
+++ src/ts-test/utils/sample-data.ts
@@ -0,0 +1,9 @@
+import { User } from '../interfaces'
+
+/** Dummy user data. */
+export const sampleUserData: User[] = [
+  { id: 101, name: 'Alice' },
+  { id: 102, name: 'Bob' },
+  { id: 103, name: 'Caroline' },
+  { id: 104, name: 'Dave', desc: "I like apple pie and memes and dreams hippity hoppity my property is on fire 🔥." },
+]
